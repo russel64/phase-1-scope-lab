ad91d3c6f9bfe2e060a90e49769c0df61f5d0e2e
William Russell <williamrussell@Williams-MacBook-Pro.local>
2023-02-02 16:01:01 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'karen';
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'kevin';
+    return leastFavoriteCustomer = 'kevin';
+}
